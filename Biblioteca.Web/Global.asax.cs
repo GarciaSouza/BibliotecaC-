dfcec2927d8ba72fceca677b1ee83c48f51dfae7
Garcia Souza <garcia.rsouza@gmail.com>
2017-06-24 01:05:26 -0300

    add Web project

---
+++ Biblioteca.Web/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using Biblioteca.Domain.Repositories;
+using Biblioteca.NH.Repositories;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Biblioteca.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+            Container container = new Container();
+            container.Register<ILivroRepository, LivroRepository>();
+            container.Register<IRevistaRepository, RevistaRepository>();
+            container.Verify();
+        }
+    }
+}
---
+++ CreateDB.sql
@@ -0,0 +1,22 @@
+﻿
+CREATE TABLE dbo.Livros (
+	ID int IDENTITY(1,1) NOT NULL,
+	ISBN nvarchar(20),
+	Titulo nvarchar(200),
+	Autor nvarchar(200),
+	DataPublicacao Datetime,
+	Versao nvarchar(10),
+	Genero nvarchar(100),
+	PalavraChave nvarchar(100),
+	Sinopse nvarchar(4000)
+)
+
+CREATE TABLE dbo.Revistas (
+	ID int IDENTITY(1,1) NOT NULL,
+	Nome nvarchar(200),
+	Numero int,
+	DataPublicacao Datetime,
+	Genero nvarchar(100),
+	PalavraChave nvarchar(100),
+	Sinopse nvarchar(4000)
+)
